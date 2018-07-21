f8314c86644e9a09f88c7daf1b7f79bbc89c9e76
dwitamam <dwitamam@gmail.com>
2018-07-21 21:56:35 +0800

    latest stable version of web2c

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,16 +13,13 @@
 
         @if(!Auth::guest())
             @if(Auth::user()->id == $post->user_id)
-        <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
             {{Form::hidden('_method', 'DELETE')}}
             {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-        {!!Form::close()!!}
-
-        <!-- Comments Form -->
-       
-
-        @endif
+            {!!Form::close()!!}
+            <!-- Comments Form -->
+            @endif
         @endif
 
 @endsection
